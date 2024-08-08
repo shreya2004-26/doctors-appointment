5f30e5631fcec4b3f332fcfd35fe355648685d49
shreya-singh <neeva02singh@gmail.com>
2024-08-08 20:07:05 +0530

    clerk integrate

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button'
-import { UserCircle } from 'lucide-react'
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
+    const { user } = useUser();
+    console.log(user);
     return (
         <div className='px-20'>
             <div className='flex justify-between py-5 px-2 items-center border-b-0 shadow-sm '>
@@ -13,8 +15,17 @@ const Header = () => {
                         <Image src='/logo.svg' width={200} height={30} className='w-[180px]' />
                     </Link>
                 </div>
-                {/* <UserCircle className='w-7 h-7' /> */}
-                <Button> Get Started</Button>
+
+                {/* If user is signed in */}
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+                {/* If user is sign out */}
+                <SignedOut>
+                    <Button>
+                        <SignInButton />
+                    </Button>
+                </SignedOut>
 
             </div>
         </div>
